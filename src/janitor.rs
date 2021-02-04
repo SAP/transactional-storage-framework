d24404da7aae5ef3b4029574d3ef96cb4bbfbf33
wvwwvwwv <wvwwvwwv@me.com>
2021-02-04 17:03:43 +0900

    chore: test repository

--- src/janitor.rs
+++ src/janitor.rs
@@ -4,7 +4,15 @@ use super::Sequencer;
 ///
 /// It manages all the active readers to calculate the minimum valid sequence.
 pub trait Janitor<S: Sequencer> {
-    /// It returns the latest database snapshot represented as a sequencer clock
+    /// Tracker is a data structure that is tagged on a snapshot.
+    ///
+    /// It enables a Janitor to track all the active readers in the system.
+    type Tracker;
+
+    /// Creates a instance out of a sequencer.
+    fn new(sequencer: &S) -> Janitor<S>;
+
+    /// It returns the latest database snapshot represented by a sequencer clock
     /// among those that are visible to all the active readers.
     fn latest_public_snapshot(&self) -> S::Clock;
 }
