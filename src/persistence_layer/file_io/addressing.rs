f887cc968041c868ee174396297a9cf4f47842f3
Changgyoo Park <changgyoo.park@sap.com>
2023-05-28 18:53:37 +0900

    fix

--- src/persistence_layer/file_io/addressing.rs
+++ src/persistence_layer/file_io/addressing.rs
@@ -15,16 +15,40 @@ pub struct Address {
 }
 
 impl Address {
-    /// The offset in a page.
+    /// Returns the offset in a page.
     #[inline]
     pub const fn page_offset(self) -> u64 {
         self.offset & ((1_u64 << PAGE_BITS) - 1)
     }
 
-    /// The page number in a segment.
+    /// Returns the page address.
     #[inline]
-    pub const fn page_number(self) -> u64 {
-        (self.offset >> PAGE_BITS) & ((1_u64 << SEGMENT_BITS) - 1)
+    pub const fn page_address(self) -> Address {
+        Self {
+            offset: self.offset & (!self.page_offset()),
+        }
+    }
+
+    /// Returns the segment address.
+    #[inline]
+    pub const fn segment_address(self) -> Address {
+        Self {
+            offset: self.offset & (!((1_u64 << (SEGMENT_BITS + PAGE_BITS)) - 1)),
+        }
+    }
+
+    /// Returns the segment directory address.
+    #[inline]
+    pub const fn segment_directory_address(self) -> Address {
+        Self {
+            offset: self.offset & (!(SEGMENT_ALIGNMENT - 1)),
+        }
+    }
+
+    /// Returns the index number of the associated segment.
+    #[inline]
+    pub const fn segment_directory_index_number(self) -> u64 {
+        (self.segment_address().offset - self.segment_directory_address().offset) / SEGMENT_SIZE
     }
 }
 
@@ -50,12 +74,15 @@ pub const SEGMENT_SIZE: u64 = 1_u64 << (SEGMENT_BITS + PAGE_BITS);
 /// The number of pages in a segment which is `4096`.
 pub const PAGES_PER_SEGMENT: usize = (SEGMENT_SIZE / PAGE_SIZE) as usize;
 
-/// The size of a segment directory is `4MB` enabling it to address `16GB` space since a segment
+/// The size of a segment directory is `2MB` enabling it to address `8GB` space since a segment
 /// addresses `4096` pages where each page is `512B`.
 ///
 /// The first page that follows the first segment directory is the database header. Segment
-/// directories are always located at `16GB` boundaries.
-pub const SEGMENT_DIRECTORY_BITS: u32 = 22;
+/// directories are always located at `8GB` boundaries.
+///
+/// The first bit of a segment directory is always `0` until the whole segments and the segment
+/// directory are permanently deleted from the database.
+pub const SEGMENT_DIRECTORY_BITS: u32 = SEGMENT_BITS + PAGE_BITS;
 
 /// The size of a segment directory.
 pub const SEGMENT_DIRECTORY_SIZE: u64 = 1_u64 << SEGMENT_DIRECTORY_BITS;
@@ -69,8 +96,9 @@ mod tests {
     use static_assertions::const_assert_eq;
 
     const_assert_eq!(SEGMENT_SIZE, 2_097_152);
-    const_assert_eq!(SEGMENT_DIRECTORY_SIZE, 4_194_304);
-    const_assert_eq!(SEGMENT_ALIGNMENT, 17_179_869_184);
+    const_assert_eq!(SEGMENT_DIRECTORY_SIZE, 2_097_152);
+    const_assert_eq!(SEGMENT_SIZE, SEGMENT_DIRECTORY_SIZE);
+    const_assert_eq!(SEGMENT_ALIGNMENT, 8_589_934_592);
 
     #[test]
     fn in_page_addressing() {
@@ -80,7 +108,32 @@ mod tests {
 
     #[test]
     fn in_segment_addressing() {
-        let address = Address::from(u64::MAX);
-        assert_eq!(address.page_number(), 0b1111_1111_1111);
+        let address = Address::from(u64::from(u32::MAX));
+        assert_eq!(
+            address.page_address().offset,
+            0b1111_1111_1111_1111_1111_1110_0000_0000
+        );
+    }
+
+    #[test]
+    fn in_database_addressing() {
+        let address = Address::from(u64::from(u32::MAX));
+        assert_eq!(
+            address.segment_address().offset,
+            0b1111_1111_1110_0000_0000_0000_0000_0000
+        );
+    }
+
+    #[test]
+    fn segment_directory_addressing() {
+        let address = Address::from(u64::from(u32::MAX) * 16);
+        assert_eq!(
+            address.segment_directory_address().offset,
+            0b1110_0000_0000_0000_0000_0000_0000_0000_0000
+        );
+        assert_eq!(
+            address.segment_directory_address().offset % SEGMENT_ALIGNMENT,
+            0
+        );
     }
 }
